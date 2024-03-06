60b9603b669ffae748efec82c4789912bac5a365
Lyranaut <78031310+Lyranaut@users.noreply.github.com>
2024-03-05 19:02:28 -0700

    End

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
+});
+
 var modal = document.getElementById("contactModal");
 var btn = document.getElementById("openModalBtn");
 var closeBtn = document.getElementsByClassName("close")[0];
@@ -8,10 +12,4 @@ btn.onclick = function() {
 
 closeBtn.onclick = function() {
     modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
