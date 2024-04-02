d8553d342d87b139b379a31b7afa40553bdf859f
Dmytro Doduliak <ddodulyak@gmail.com>
2024-04-02 04:04:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Получаем элемент, в котором будет меняться текст профессии
+const professionElement = document.getElementById("profession");
+
+// Массив фраз для смены
+const phrases = [
+    "Python Developer",
+    "Web Developer",
+    "System Administrator",
+    "Data Entry Specialist"
+];
+
+// Функция для смены текста
+function changeProfession() {
+    let index = 0;
+    setInterval(() => {
+        professionElement.textContent = phrases[index];
+        index = (index + 1) % phrases.length;
+    }, 2000); // Каждые 3 секунды меняем текст
+}
+
+// Вызываем функцию для смены текста
+changeProfession();
